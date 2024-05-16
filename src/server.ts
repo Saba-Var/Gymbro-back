d61a0fa782b632a7449b5383cd940af25a5e9d7e
Saba-Var <vartasashvili94@gmail.com>
2024-05-16 23:45:01 +0400

    feat: create error handler middliware

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import { errorHandler } from 'middlewares/errorHandler'
+import { NotFoundError } from 'errors/NotFoundError'
 import express from 'express'
 import 'express-async-errors'
 import dotenv from 'dotenv'
@@ -10,8 +12,13 @@ server.get('/', (_req, res) => {
   res.send('Hello World!')
 })
 
-const PORT = process.env.PORT
+server.all('*', () => {
+  throw new NotFoundError()
+})
 
+server.use(errorHandler)
+
+const PORT = process.env.PORT
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is running on http://localhost:${PORT}`)
