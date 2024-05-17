dd68041bf9e6b997f4ef4e0c08edfd71128508cf
Saba-Var <vartasashvili94@gmail.com>
2024-05-17 22:48:55 +0400

    feat: setup test configuration

--- src/config/startServer.ts
+++ src/config/startServer.ts
@@ -3,10 +3,10 @@ import connectToMongoDB from 'config/mongoDB'
 import type { Express } from 'express'
 
 export const startServer = async (server: Express) => {
-  const PORT = process.env.PORT
+  const SERVER_PORT = process.env.SERVER_PORT
 
-  server.listen(PORT, async () => {
+  server.listen(SERVER_PORT, async () => {
     await connectToMongoDB()
-    console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`Server is running on http://localhost:${SERVER_PORT}`)
   })
 }
--- src/utils/errorLoggerHandler.ts
+++ src/utils/errorLoggerHandler.ts
@@ -5,13 +5,15 @@ import 'winston-daily-rotate-file'
 export const errorLoggerHandler = (req: Request, error: Error) => {
   const message = error?.message || 'Something went wrong'
 
-  winstonLogger.error(message, {
-    user: null, // TODO: Add user
-    method: req?.method,
-    params: req?.params,
-    query: req?.query,
-    stack: error?.stack,
-    body: req?.body,
-    url: req?.url,
-  })
+  if (!req?.url.includes('favicon.ico')) {
+    winstonLogger.error(message, {
+      user: null, // TODO: Add user
+      method: req?.method,
+      params: req?.params,
+      query: req?.query,
+      stack: error?.stack,
+      body: req?.body,
+      url: req?.url,
+    })
+  }
 }
