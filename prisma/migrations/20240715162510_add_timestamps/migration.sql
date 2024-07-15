69ba5083d9b4cea0dac368e03f697f27d54baefd
Saba-Var <vartasashvili94@gmail.com>
2024-07-15 20:25:36 +0400

    chore: add timestamps

---
+++ prisma/migrations/20240715162510_add_timestamps/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Company` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Permission` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Role` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `RolePermission` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Staff` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `StaffPermission` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `Company` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Permission` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Role` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `RolePermission` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Staff` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `StaffPermission` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
