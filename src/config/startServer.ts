35cc4bb4cefdf77459ecab04703899261a67d99d
Saba-Var <vartasashvili94@gmail.com>
2024-07-15 19:59:59 +0400

    chore: remove mongodb

--- src/config/startServer.ts
+++ src/config/startServer.ts
@@ -1,12 +1,10 @@
 /* eslint-disable no-console */
-import connectToMongoDB from 'config/mongoDB'
 import type { Express } from 'express'
 
 export const startServer = async (server: Express) => {
   const SERVER_PORT = process.env.SERVER_PORT
 
   server.listen(SERVER_PORT, async () => {
-    await connectToMongoDB()
     console.log(`Server is running on http://localhost:${SERVER_PORT}`)
   })
 }
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,26 +1,5 @@
-import { MongoMemoryServer } from 'mongodb-memory-server'
-import mongoose from 'mongoose'
+beforeAll(async () => {})
 
-let mongo: MongoMemoryServer
+beforeEach(async () => {})
 
-beforeAll(async () => {
-  mongo = await MongoMemoryServer.create()
-  const mongoUri = mongo.getUri()
-
-  await mongoose.connect(mongoUri, {})
-})
-
-beforeEach(async () => {
-  const collections = await mongoose.connection.db.collections()
-
-  for (const collection of collections) {
-    await collection.deleteMany({})
-  }
-})
-
-afterAll(async () => {
-  if (mongo) {
-    await mongo.stop()
-  }
-  await mongoose.connection.close()
-})
+afterAll(async () => {})
