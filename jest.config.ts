e2001b3ac69a620867f9ea712419f11ce39dd6f7
Saba-Var <vartasashvili94@gmail.com>
2024-07-17 11:49:36 +0400

    chore: rename test folder to testing

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,10 @@ import type { Config } from 'jest'
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./src/test/jest.setup.ts'],
+  setupFilesAfterEnv: ['./src/testing/jest.setup.ts'],
   moduleDirectories: ['node_modules', 'src'],
   testMatch: ['**/?(*.)+(spec|test).[t]s?(x)'],
-  setupFiles: ['<rootDir>/src/test/setEnvVars.ts'],
+  setupFiles: ['<rootDir>/src/testing/setEnvVars.ts'],
 }
 
 export default config
