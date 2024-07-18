898a53943d3435b4fcd23372d4610e32b3e9bee7
Saba-Var <vartasashvili94@gmail.com>
2024-07-18 13:32:13 +0400

    fix: local testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,17 @@
 import type { Config } from 'jest'
 
+const setupFiles =
+  process.env.NODE_ENV === 'local-testing'
+    ? ['<rootDir>/src/testing/setEnvVars.ts']
+    : []
+
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./src/testing/jest.setup.ts'],
   moduleDirectories: ['node_modules', 'src'],
   testMatch: ['**/?(*.)+(spec|test).[t]s?(x)'],
+  setupFiles,
 }
 
 export default config
--- src/testing/setEnvVars.example.ts
+++ src/testing/setEnvVars.example.ts
@@ -2,7 +2,12 @@
 // 1. Create a copy of this file and rename it to setEnvVars.ts
 // 2. Fill in the values for the environment variables
 
-// process.env.SERVER_PORT =
-// process.env.DATABASE_URL =
-// process.env.SUPER_USER_EMAIL =
-// process.env.SUPER_USER_PASSWORD =
+// const setEnvForLocalTesting = () => {
+//   const envObj: typeof process.env = {}
+
+//   for (const key in envObj) {
+//     process.env[key] = envObj[key]
+//   }
+// }
+
+// setEnvForLocalTesting()
